a69f9407d2e0d984f12e71bc8efaef926eaa2216
Adam <Adam@sigterm.info>
2025-06-01 22:26:08 -0400

    run pngcrush/optipng

--- pack-assets.sh
+++ pack-assets.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+for file in assets/png/*.png; do
+  pngcrush -reduce -rem allb -ow $file
+  optipng -clobber $file
+done
+
 zip -r -j assets.zip LICENSE-GRAPHICS assets/png/*.png >/dev/null
 sha256sum assets.zip 2>&1
